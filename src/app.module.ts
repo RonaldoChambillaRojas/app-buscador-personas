584742679ca186d6a1213f6c07f1919fd4025dd2
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-12-22 23:58:09 -0500

    chore: update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    // 1. Configurar variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    // 2. Configurar TypeORM Asincrono (para leer las env vars)
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // iOJO! Solo para desarrollo (crea tablas auto)
+      }),
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
